5dc41d0c3ae827786bcea988ef22d4e2c8058d06
Maks <maks_mun@bk.ru>
2024-03-15 21:21:19 +0500

    Create main page view

--- freelance/views.py
+++ freelance/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from typing import Any
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import Service, Order, Executor, Customer
 
-# Create your views here.
+class MainPageView(TemplateView):
+    template_name = 'basic/index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'Main page'
+        return context
