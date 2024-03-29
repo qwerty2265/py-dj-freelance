b394dbb21cf7ec0a48f5cf59bc23cdc725cb3c87
Maks <maks_mun@bk.ru>
2024-03-29 21:37:30 +0500

    Create order create and edit view

--- freelance/views.py
+++ freelance/views.py
@@ -22,3 +22,50 @@ class MainPageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["message"] = "Это главная страница проекта"
         return context
+    
+class OrderCreateView(LoginRequiredMixin, CreateView):
+    template_name = "freelance/orders/order_create.html"
+    form_class = OrderForm
+    success_url = reverse_lazy(
+        "freelance:order-success"
+    )  # URL для перенаправления после успешного создания
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title_label"] = "Создание заказа"
+        return context
+
+    def form_valid(self, form):
+        # Получаем объект Customer для текущего пользователя
+        customer_profile, created = Customer.objects.get_or_create(
+            user=self.request.user
+        )
+        form.instance.customer = customer_profile
+        return super().form_valid(form)
+
+
+class OrderEditView(LoginRequiredMixin, UpdateView):
+    template_name = "freelance/orders/order_create.html"
+    form_class = OrderForm
+    model = Order
+    success_url = reverse_lazy(
+        "freelance:order-success"
+    )  # URL для перенаправления после успешного создания
+
+    def get_context_data(self, **kwargs):
+        if (
+            Order.objects.get(id=self.kwargs["pk"]).customer == None or self.request.user != Order.objects.get(id=self.kwargs["pk"]).customer.user
+            or not self.request.user.is_authenticated 
+        ):
+            raise PermissionDenied
+        context = super().get_context_data(**kwargs)
+        context["title_label"] = "Редактирование заказа"
+        return context
+
+    def form_valid(self, form):
+        # Получаем объект Customer для текущего пользователя
+        customer_profile, created = Customer.objects.get_or_create(
+            user=self.request.user
+        )
+        form.instance.customer = customer_profile
+        return super().form_valid(form)
