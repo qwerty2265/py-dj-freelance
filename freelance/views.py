dee0cfa5f136ab7258a5f85bfb41849a867821ad
Maks <maks_mun@bk.ru>
2024-03-29 21:36:44 +0500

    Create custom logout view

--- freelance/views.py
+++ freelance/views.py
@@ -1,11 +1,24 @@
-from typing import Any
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    TemplateView,
+    CreateView,
+    UpdateView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.views import LogoutView
+from django.urls import reverse_lazy
+
 from .models import Service, Order, Executor, Customer
 
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy("login")
+
 class MainPageView(TemplateView):
-    template_name = 'basic/index.html'
-    
+    template_name = "basic/index.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['message'] = 'Main page'
+        context["message"] = "Это главная страница проекта"
         return context
