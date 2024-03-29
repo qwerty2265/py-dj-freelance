b1926db04373706a84e4d11ab86883b14d88e9d5
Maks <maks_mun@bk.ru>
2024-03-29 21:26:39 +0500

    Add login path to config

--- config/urls.py
+++ config/urls.py
@@ -19,10 +19,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.auth import views as auth_views
+
 from config.ddtb import DEBUG_TOOLBAR_PATH
 
 urlpatterns = [
-       
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('', include('freelance.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
