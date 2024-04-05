538e99442f9f73498752d6f9632bf37a12893ed7
Maks <maks_mun@bk.ru>
2024-04-05 21:37:40 +0500

    Create urls

--- freelance/urls.py
+++ freelance/urls.py
@@ -1,6 +1,33 @@
 from django.urls import path
-from .views import MainPageView
+from .views import (
+    MainPageView,    
+    ServiceListView,
+    ServiceDetailView,
+    OrderListView,
+    OrderDetailView,
+    ExecutorListView,
+    ExecutorDetailView,
+    CustomerListView,
+    CustomerDetailView,
+    CustomLogoutView,
+    OrderListView,
+    OrderDetailView,
+    OrderCreateView,
+    OrderEditView,
+)
 
-urlpatters = [
-    path('', MainPageView.as_view(), name='main_page'),
+urlpatterns = [
+    path("", MainPageView.as_view(), name="main_page"),
+    path("executors/", ExecutorListView.as_view(), name="executor-list"),
+    path("executors/<int:pk>/", ExecutorDetailView.as_view(), name="executor-detail"),
+    path("customers/", CustomerListView.as_view(), name="customer-list"),
+    path("customers/<int:pk>/", CustomerDetailView.as_view(), name="customer-detail"),
+    path("services/", ServiceListView.as_view(), name="service-list"),
+    path("services/<int:pk>/", ServiceDetailView.as_view(), name="service-detail"),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path("orders/create/", OrderCreateView.as_view(), name="order-create"),
+    path("orders/edit/<int:pk>", OrderEditView.as_view(), name="order-edit"),
+    path("orders/", OrderListView.as_view(), name="order-list"),
+    path("orders/<int:pk>/", OrderDetailView.as_view(), name="order-detail"),
+    path("orders/success/", OrderListView.as_view() , name="order-success"),
 ]
