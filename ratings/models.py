d04c6c16c595bc56e6d1398ab78daf5ffb8034ca
Maks <maks_mun@bk.ru>
2024-06-06 01:46:33 +0500

    Create rating order model

--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from freelance.models import Order, UserProfile
 
-# Create your models here.
+class RatingOrder(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    testimonial = models.TextField()
+    order_rating = models.FloatField()
+
+    class Meta:
+        unique_together = ("order", "user")
+
+    def __str__(self):
+        return f"Rating for Order {self.order} by {self.user}"
