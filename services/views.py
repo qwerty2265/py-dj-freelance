ffc733dac87f227b27f57e5c1c19b181da4edbef
Maks <maks_mun@bk.ru>
2024-06-06 20:49:51 +0500

    Create service detail view

--- services/views.py
+++ services/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Service
 from .forms import ServiceForm
 
@@ -7,6 +7,12 @@ class ServiceListView(ListView):
     template_name = 'services/service_list.html'
     context_object_name = 'services'
     extra_context = {'title': 'Список услуг'}
+
+class ServiceDetailView(DetailView):
+    model = Service
+    template_name = 'services/service_detail.html'
+    context_object_name = 'service'
+    extra_context = {'title': 'Детали услуги'}
 
 class ServiceCreateView(CreateView):
     model = Service
