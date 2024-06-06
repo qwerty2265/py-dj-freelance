2702fa713d791eb6baa50fb330ad64c086089637
Maks <maks_mun@bk.ru>
2024-06-06 21:01:16 +0500

    Register testimonial admin

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import RatingOrder
+from .models import RatingOrder, Testimonial
 
 @admin.register(RatingOrder)
 class RatingOrderAdmin(admin.ModelAdmin):
     list_display = ('order', 'user', 'order_rating', 'testimonial')
     search_fields = ('order__title', 'user__user__username', 'testimonial')
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('order', 'user', 'testimonial')
+    search_fields = ('user__username', 'order__id')
