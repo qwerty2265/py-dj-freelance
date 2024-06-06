021ce619026259e7a23b45f36380861b606cb3e7
Maks <maks_mun@bk.ru>
2024-06-06 20:43:04 +0500

    Create service model

--- services/models.py
+++ services/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Название услуги")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    image = models.ImageField(upload_to='services/', verbose_name="Изображение", blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
+
+    def __str__(self):
+        return self.title
