b72f4346aba551013b7bf553a9f4f4c494e5f925
Maks <maks_mun@bk.ru>
2024-06-06 20:51:18 +0500

    Update service urls

--- services/urls.py
+++ services/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from .views import ServiceListView, ServiceDetailView, ServiceCreateView, ServiceUpdateView
 
 urlpatterns = [
-    path('', views.service_list, name='service_list'),
-    path('create/', views.service_create, name='service_create'),
-    path('edit/<int:pk>/', views.service_edit, name='service_edit'),
+    path('', ServiceListView.as_view(), name='service_list'),
+    path('<int:pk>/', ServiceDetailView.as_view(), name='service_detail'),
+    path('create/', ServiceCreateView.as_view(), name='service_create'),
+    path('edit/<int:pk>/', ServiceUpdateView.as_view(), name='service_edit'),
 ]
