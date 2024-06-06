4df2958c32c646e4ee3c5b13155632b90f5fb3b1
Maks <maks_mun@bk.ru>
2024-06-06 20:42:54 +0500

    Create service views

--- services/views.py
+++ services/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Service
+from .forms import ServiceForm
 
-# Create your views here.
+def service_list(request):
+    services = Service.objects.all()
+    return render(request, 'services/service_list.html', {'services': services, 'title': 'Список услуг'})
+
+def service_create(request):
+    if request.method == "POST":
+        form = ServiceForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('service_list')
+    else:
+        form = ServiceForm()
+    return render(request, 'services/service_form.html', {'form': form, 'title': 'Создание услуги'})
+
+def service_edit(request, pk):
+    service = get_object_or_404(Service, pk=pk)
+    if request.method == "POST":
+        form = ServiceForm(request.POST, request.FILES, instance=service)
+        if form.is_valid():
+            form.save()
+            return redirect('service_list')
+    else:
+        form = ServiceForm(instance=service)
+    return render(request, 'services/service_form.html', {'form': form, 'title': 'Редактирование услуги'})
